d05a5ce6440f8f3a7ba56369b2e9c1c5f3aa8241
mykolapasko <nikpasko@gmail.com>
2020-08-05 14:44:28 +0300

    mongoose warning fixed, fake weighting process added to the agent module

--- src/public/agent/agent.deck.controller.js
+++ src/public/agent/agent.deck.controller.js
@@ -4,8 +4,8 @@
 angular.module('public')
 .controller('DeckController', DeckController);
 
-DeckController.$inject = ['$stateParams','agents', 'DataService'];
-function DeckController($stateParams, agents, DataService) {
+DeckController.$inject = ['$stateParams','agents', 'DataService', 'CalculationService'];
+function DeckController($stateParams, agents, DataService, CalculationService) {
   var deckCtrl = this;
   deckCtrl.agents = agents.filter(function(agent) {
   	return agent.deck === $stateParams.deck;
@@ -13,18 +13,34 @@ function DeckController($stateParams, agents, DataService) {
 
   deckCtrl.color = $stateParams.deck;
 
-  deckCtrl.getWeightAndPutInfoAfter = function(agent, index) {
-    var promise = DataService.getAgentWeight();
-    promise.then(function(response) {
-      var weight = parseFloat(response.slice(2,8));
-      agent.weight = weight;
-      agent.isEmpty = false;
-      agent.data = {};
-      agent.data.weight = weight;
-      agent.data.isEmpty = agent.isEmpty;
-      DataService.putInfoAgents(agent);
-    });
-  }
+
+// This method is for the production
+
+  // deckCtrl.getWeightAndPutInfoAfter = function(agent, index) {
+  //   var promise = DataService.getAgentWeight();
+  //   promise.then(function(response) {
+  //     var weight = parseFloat(response.slice(2,8));
+  //     agent.weight = weight;
+  //     agent.isEmpty = false;
+  //     agent.data = {};
+  //     agent.data.weight = weight;
+  //     agent.data.isEmpty = agent.isEmpty;
+  //     DataService.putInfoAgents(agent);
+  //   });
+  // }
+
+
+// This method is for the development process
+
+  deckCtrl.getFakeWeightAndPutInfoAfter = function(agent, index) {
+    var fakeAgentWgt = parseFloat(CalculationService.getRandomArbitrary(159.5, 164.3).toPrecision(4));
+    agent.data = {};
+    agent.weight = fakeAgentWgt;
+    agent.isEmpty = false;
+    agent.data.weight = fakeAgentWgt;
+    agent.data.isEmpty = agent.isEmpty;
+    DataService.putInfoAgents(agent);
+}
 
 
 
